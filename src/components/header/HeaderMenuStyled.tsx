d8807f2cf94f79c8d45c7c42d9f889dd96339562
RafaelHenkel <rafael0henkel1@gmail.com>
2024-08-04 14:03:53 -0300

    feat(root): add background

--- src/components/header/HeaderMenuStyled.tsx
+++ src/components/header/HeaderMenuStyled.tsx
@@ -6,6 +6,28 @@ const HeaderMenuStyled = styled.ul`
   align-items: center;
   font-size: 1.1rem;
   gap: 2rem;
+  li {
+    transition-duration: 0.3s;
+    position: relative;
+  }
+  li:focus:after,
+  li:hover:after {
+    width: 100%;
+    left: 0%;
+  }
+  li:after {
+    content: '';
+    pointer-events: none;
+    bottom: -2px;
+    left: 50%;
+    position: absolute;
+    width: 0%;
+    height: 2px;
+    background-color: #a7a7a7;
+    transition-timing-function: cubic-bezier(0.25, 0.8, 0.25, 1);
+    transition-duration: 400ms;
+    transition-property: width, left;
+  }
   @media (max-width: 1024px) {
     display: none;
   }
